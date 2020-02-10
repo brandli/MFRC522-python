02a1b61d60f5df343b441b8dbfcd011aca48daff
Christian <ch.braendli@gmail.com>
2020-02-11 00:07:12 +0100

    Test

--- MFRC522.py
+++ MFRC522.py
@@ -111,6 +111,7 @@ class MFRC522:
     device = []
 
     def __init__(self, dev='/dev/spidev0.0', spd=1000000):
+        print(dev)
         self.device = spi.openSPI(device=dev, mode=0, speed=spd, bits=8, delay=0)
         #    GPIO.setmode(GPIO.BOARD)
         #    GPIO.setup(22, GPIO.OUT)
